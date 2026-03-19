64a7134567543071c4da4b9af9041dc9d1d650cb
Kiran Jaiswal <kiranjaiswalkj2002@gmail.com>
2026-03-19 12:55:47 +0530

    initial Commit

---
+++ src/test/java/com/example/Smart/Study/Planner/SmartStudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Smart.Study.Planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
